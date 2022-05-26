9a8a49fcc42e11b9b551f9e86ecc9f6d8c8332ea
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-05-26 21:47:45 +0500

    11

--- js/index2.js
+++ js/index2.js
@@ -1,1 +1,18 @@
-console.log("index 2");
+const btn = document.querySelector ('.btn');
+const close__block = document.querySelector ('.close__block');  
+const modal = document.querySelector ('.modal');
+const wrapper = document.querySelector ('.wrapper');
+
+btn.addEventListener("click",modalOpen)
+close__block.addEventListener("click",modalClose)
+
+function modalClose () {
+    console.log("none");
+    modal.style.display = "none"
+}
+    
+
+
+function modalOpen () {
+modal.style.display = "block"
+}
