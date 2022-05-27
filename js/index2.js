9aeb8c50200c366f4fb6dbfb8ae1e38071ae6d16
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-05-27 18:58:53 +0500

    prowerka

--- js/index2.js
+++ js/index2.js
@@ -2,9 +2,14 @@ const btn = document.querySelector ('.btn');
 const close__block = document.querySelector ('.close__block');  
 const modal = document.querySelector ('.modal');
 const wrapper = document.querySelector ('.wrapper');
+const modal__input = document.querySelector ('.modal__input');
+const ssylka = document.querySelector ('.ssylka');
+const modal__btn = document.querySelector ('.modal__btn');
 
 btn.addEventListener("click",modalOpen)
 close__block.addEventListener("click",modalClose)
+
+modal__btn.addEventListener('click',checkInput)
 
 function modalClose () {
     console.log("none");
@@ -13,4 +18,15 @@ function modalClose () {
     
 function modalOpen () {
 modal.style.display = "block"
+}
+
+function checkInput (){
+   const kod = modal__input.value
+   console.log(kod);
+   if (kod == 0000){
+    ssylka.click()
+   }
+   else {
+       alert("Kod Neverniy")
+   }
 }
