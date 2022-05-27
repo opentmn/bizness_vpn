ed1a97cb141a84b09bb130661d72a3d51c0fd241
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-05-27 19:33:35 +0500

    password

--- js/index2.js
+++ js/index2.js
@@ -1,32 +1,33 @@
-const btn = document.querySelector ('.btn');
-const close__block = document.querySelector ('.close__block');  
-const modal = document.querySelector ('.modal');
-const wrapper = document.querySelector ('.wrapper');
-const modal__input = document.querySelector ('.modal__input');
-const ssylka = document.querySelector ('.ssylka');
-const modal__btn = document.querySelector ('.modal__btn');
+const btn = document.querySelector(".btn");
+const close__block = document.querySelector(".close__block");
+const modal = document.querySelector(".modal");
+const wrapper = document.querySelector(".wrapper");
+const modal__input = document.querySelector(".modal__input");
+const ssylka = document.querySelector(".ssylka");
+const modal__btn = document.querySelector(".modal__btn");
+const password = new Date().getDate();
+console.log(password);
 
-btn.addEventListener("click",modalOpen)
-close__block.addEventListener("click",modalClose)
+btn.addEventListener("click", modalOpen);
+close__block.addEventListener("click", modalClose);
 
-modal__btn.addEventListener('click',checkInput)
+modal__btn.addEventListener("click", checkInput);
 
-function modalClose () {
-    console.log("none");
-    modal.style.display = "none"
-}
-    
-function modalOpen () {
-modal.style.display = "block"
+function modalClose() {
+  console.log("none");
+  modal.style.display = "none";
 }
 
-function checkInput (){
-   const kod = modal__input.value
-   console.log(kod);
-   if (kod == 0000){
-    ssylka.click()
-   }
-   else {
-       alert("Kod Neverniy")
-   }
+function modalOpen() {
+  modal.style.display = "block";
+}
+
+function checkInput() {
+  const kod = modal__input.value;
+  console.log(kod);
+  if (kod == password) {
+    ssylka.click();
+  } else {
+    alert("Kod Neverniy");
+  }
 }
